d17825ae01a42a8ba00055cceb3dc10d7fa26f95
ranguangyu <ranguangyu@zbj.com>
2016-10-08 17:29:47 +0800

    component-css

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,12 +5,12 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
-    lessOptions: {
-      paths: [
-        'app/styles/app'
-      ],
-      sourceMap: false
-    },
+    // lessOptions: {
+    //   paths: [
+    //     'app/styles/app'
+    //   ],
+    //   sourceMap: false
+    // },
     babel: {
       comments: false,
       optional: ['es7.decorators'],
@@ -44,7 +44,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');console.log('kkkkkkkk');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
 
   app.import('vendor/lodash/lodash.min.js');

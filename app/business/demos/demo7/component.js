a08506d8901d452508209268974ec3c99b6ca53a
ranguangyu <ranguangyu@zbj.com>
2016-10-11 19:08:10 +0800

    test

--- app/business/demos/demo7/component.js
+++ app/business/demos/demo7/component.js
@@ -1,21 +1,41 @@
 import Ember from 'ember';
+import computed from 'ember-computed-decorators';
 
 export default Ember.Component.extend({
+
+	@computed('newUser.id')
+	listenNewUser() {
+		let newUser = this.get('newUser');
+		console.log(newUser);
+	},
+
+	newUser: {},
+
 	actions: {
 		add(user) {
+			// console.log(user);
 			let users = this.get('model.users');
             let cuser = _.cloneDeep(user);
             users.pushObject(cuser);
-            user = {
-                id: '',
-                name: '',
-                age: ''
-            };
-            this.set('model.newUser', user);
+            // user = {
+            //     id: '',
+            //     name: '',
+            //     age: ''
+            // };
+            // this.set('model.newUser', user);
 		},
         del(user) {
             let users = this.get('model.users');
             users.removeObject(user);
-        }
+        },
+		press() {
+			let users = this.get('model.users');
+            let cuser = {
+                id: '11',
+                name: '22',
+                age: '333'
+            };
+			users.pushObject(cuser);
+		}
 	}
 });
